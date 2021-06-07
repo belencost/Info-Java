3ef03519928535705bf17bc79eb16c9e0c860bb7
belencost <belencostantin@gmail.com>
2021-06-07 20:29:50 -0300

    Nivel1Completo

---
+++ Nivel 1/Ejercicio8.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Ejercicio8{
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Ingrese nombre y apellido: ");
+    String nombreyA = scan.nextLine();
+    System.out.println("Edad: ");
+    int edad = scan.nextInt();
+    System.out.println("Direccion: ");
+    String direccion = scan.nextLine();
+    System.out.println("Ciudad: ");
+    String ciudad = scan.nextLine();
+    Persona persona1 = new Persona(nombreyA, edad, direccion, ciudad);
+    scan.close();
+    System.out.println(persona1);
+    }
+}
+class Persona{
+   String nombreyA;
+   int edad;
+   String direccion;
+   String ciudad;
+
+   public Persona(String nombreyA, int edad, String direccion, String ciudad){
+       this.nombreyA = nombreyA;
+       this.edad = edad;
+       this.direccion = direccion;
+       this.ciudad = ciudad;
+   }
+   @Override
+   public String toString() {
+       return ciudad + " - " + direccion + " - " + edad + " - " + nombreyA;
+   }
+}
---
+++ Nivel 1/Ejercicio9.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections; 
+import java.util.List;
+
+
+public class Ejercicio9{
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Ingrese una palabra: ");
+        String palabra = scan.nextLine();
+        System.out.println("Ingrese una letra: ");
+        String letra = scan.nextLine();
+        int cant = 0;
+
+        List<String> Listpalabra = Arrays.asList(palabra.split(""));
+
+        for(int i = 0; i < palabra.length(); i++){
+            
+            cant = Collections.frequency(Listpalabra, letra); }
+        
+        System.out.println(cant);
+        scan.close();
+    }
+    
+}
