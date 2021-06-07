ce01256b02fef1f9ad8dae5ed443042280732d2a
belencost <belencostantin@gmail.com>
2021-06-07 17:36:31 -0300

    Ejercicio1modificado

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -4,7 +4,7 @@ public class Ejercicio1{
 
     public static void main(String[] args){
         Scanner scan = new Scanner (System.in);
-        System.out.println(" Ingrese un nombre ");ol
+        System.out.println(" Ingrese un nombre ");
         String usuario = scan.nextLine();
         System.out.println("HOLA " + usuario + " !!!");
         scan.close();
